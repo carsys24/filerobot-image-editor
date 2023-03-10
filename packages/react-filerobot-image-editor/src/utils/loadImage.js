787293005ee00f4fd775cb4d14f15ef4e5e1b0c8
Mohammed K <53986586+mehamednews@users.noreply.github.com>
2023-03-10 19:56:35 +0100

    set imageElement.crossOrigin to use-credentials

--- packages/react-filerobot-image-editor/src/utils/loadImage.js
+++ packages/react-filerobot-image-editor/src/utils/loadImage.js
@@ -1,21 +1,17 @@
 import extractNameFromUrl from './extractNameFromUrl';
 
 const loadImage = (imageSrc, imageFileName) =>
-  new Promise((resolve, reject) => {
-    const imageElement = new Image();
-    imageElement.crossOrigin = 'Anonymous';
-    imageElement.src = imageSrc;
-    imageElement.name = imageFileName ?? extractNameFromUrl(imageSrc);
-    imageElement.onload = () => {
-      resolve(imageElement);
-    };
-    imageElement.onerror = () => {
-      reject(
-        new Error(
-          `Error in loading the image with the provided url: ${imageSrc}`,
-        ),
-      );
-    };
-  });
+	new Promise((resolve, reject) => {
+		const imageElement = new Image();
+		imageElement.crossOrigin = 'use-credentials';
+		imageElement.src = imageSrc;
+		imageElement.name = imageFileName ?? extractNameFromUrl(imageSrc);
+		imageElement.onload = () => {
+			resolve(imageElement);
+		};
+		imageElement.onerror = () => {
+			reject(new Error(`Error in loading the image with the provided url: ${imageSrc}`));
+		};
+	});
 
 export default loadImage;
